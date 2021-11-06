f70312c4475dc199de4d46cbdb569c4d3e8ba4db
Axel <axosar@gmail.com>
2021-11-05 20:09:18 -0600

    scene, camera and renderer

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,34 +2,41 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xff0000)
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+const camera1 = new THREE.PerspectiveCamera(
+    30,
+    1,
+    .1,
+    10000
 )
-camera.position.z = 2
+const camera2 = new THREE.OrthographicCamera(-2,2,2,-2)
+camera1.position.z = 2
+camera2.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+new OrbitControls(camera1, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
+//const geometry = new THREE.BoxGeometry()
+const geometry = new THREE.TorusKnotGeometry()
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
+    color: 0xffffff,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
+cube.scale.x = .2
+cube.scale.y = .2
+cube.scale.z = .2
 scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
+    camera1.aspect = window.innerWidth / window.innerHeight
+    camera1.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
@@ -44,7 +51,7 @@ function animate() {
 }
 
 function render() {
-    renderer.render(scene, camera)
+    renderer.render(scene, camera1)
 }
 
 animate()
